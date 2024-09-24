8632dfa694aa6be2d96c45e82e7e1137de8040a7
Amir Olagunju <amirolagunju14@Amirs-MacBook-Air-4.local>
2024-09-23 19:19:55 -0600

    first commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,95 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>University of Alberta - Edmonton, AB</li>
+        <li>Webber Academy - Calgary, AB</li>
+        
+      </ul>
+    ),
+  },
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>PostgreSQL</li>
+        <li>Sequelize</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>AWS Cloud Practitioner</li>
+        <li>Google Professional Cloud Developer</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("education");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+          I am currently pursuing a Mechanical Engineering degree at the University of Alberta, enhancing myself to engage in innovative industries around the world. My journey in programming has led me to develop proficient skills in Python, Java and C++. My passion for engineering goes beyond code, as I continue to increase my knowledge on current topics in the everchanging engineering world.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/Experience.jsx
@@ -0,0 +1,51 @@
+const Experience = () => {
+    return (
+        <section className="text-white " id="about">
+            {/* Section Title */}
+            <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+                Experience
+            </h2>
+            {/* Third Experience Section */}
+            <div className="mb-8">
+                <h3 className="font-bold text-lg mb-2">
+                    Mechanical Member, Autonomous Robotic Vehicle Project Club – Edmonton, AB
+                </h3>
+                <p className="text-sm text-gray-300 mb-4">January 2024 – April 2024</p>
+                <ul className="list-disc pl-6 space-y-2">
+                    <li>Collaborated with team members to design and build underwater robotic vehicles for competition.</li>
+                    <li>Participated in weekly meetings to discuss project progress, troubleshoot issues, and plan the next steps.</li>
+                    <li>Acquired proficiency in SolidWorks to craft designs intended for subsequent 3D printing.</li>
+                </ul>
+            </div>
+            {/* First Experience Section */}
+            <div className="mb-8">
+                <h3 className="font-bold text-lg mb-2">
+                    Apprentice Cook, Calgary Stampede – Calgary, AB
+                </h3>
+                <p className="text-sm text-gray-300 mb-4">June 2022 – August 2022</p>
+                <ul className="list-disc pl-6 space-y-2">
+                    <li>Executed multiple chefs' orders efficiently, maintaining high-quality standards in a fast-paced environment.</li>
+                    <li>Ensured prompt and accurate delivery of up to 4000 customers' orders while keeping customer satisfaction.</li>
+                    <li>Managed and resolved customers' order issues promptly and professionally.</li>
+                </ul>
+            </div>
+
+            {/* Second Experience Section */}
+            <div className="mb-8">
+                <h3 className="font-bold text-lg mb-2">
+                    Volunteer, Calgary Public Library – Calgary, AB
+                </h3>
+                <p className="text-sm text-gray-300 mb-4">July 2022 – August 2022</p>
+                <ul className="list-disc pl-6 space-y-2">
+                    <li>Facilitated children's reading development by providing individualized support and encouragement.</li>
+                    <li>Taught coding techniques on Scratch to children, fostering their interest and proficiency in technology.</li>
+                    <li>Organized activities and reading sessions to promote learning and reading among children.</li>
+                </ul>
+            </div>
+
+
+        </section>
+    );
+};
+
+export default Experience;
